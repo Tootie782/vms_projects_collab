8a4c03be5882e992e51c6ad71853b8472a480f1c
Oscar <o.aguayo02@ufromail.cl>
2023-08-02 18:20:04 -0400

    Se agregan dependencias, Dockerfile y se agrega conexión a BD

---
+++ Dockerfile
@@ -0,0 +1,11 @@
+FROM python:3.9
+
+EXPOSE 10000
+
+WORKDIR /service
+
+COPY . ./
+
+RUN pip install -r dependences.txt
+
+ENTRYPOINT ["uvicorn", "main:app", "--port", "10000", "--host", "0.0.0.0"]
--- src/db_connector.py
+++ src/db_connector.py
@@ -1,15 +1,23 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from dotenv import load_dotenv
+import json
 import os
 
 load_dotenv()
 #postgresql://myuser:mypassword@localhost:5432/mydatabase Azure Key Vault
-DATABASE_URL = os.getenv("DATABASE_URL")
+with open('credentials.json', 'r') as f:
+    credentials = json.load(f)
 
-engine = create_engine(
-    DATABASE_URL
-)
+user = credentials.get("user")  # Asegúrate de agregar el campo 'user' en tu JSON
+password = credentials.get("password")
+database = credentials.get("database")
+host = credentials.get("host")
+
+# Construye la URL de la base de datos
+DATABASE_URL = f"postgresql://{user}:{password}@{host}:5432/{database}"
+
+engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 def get_db():
